ff33fd8052eae5142a330ae42914991d7f4f23e0
Deepankar <deepankar7493@gmail.com>
2025-10-30 19:15:51 +0530

    attempting to make project arrays dynamically that holds tasks

--- src/data.ts
+++ src/data.ts
@@ -19,6 +19,33 @@ class MyTask {
     this.priority = priority;
   }
 }
+interface ArraysObject {
+  [key: string]: object[];
+}
+
+class ProjectsArray {
+  arrays: ArraysObject = {};
+  activeArrayName: string;
+
+  constructor(defaultArrayName: string) {
+    this.activeArrayName = defaultArrayName;
+    this.arrays[defaultArrayName] = [];
+  }
+
+  setActiveArray(name: string) {
+    if (!this.arrays[name]) {
+      this.arrays[name] = [];
+    }
+    this.activeArrayName = name;
+  }
+
+  addValueToActiveArray(value: object) {
+    this.arrays[this.activeArrayName].push(value);
+    console.log(this.arrays[this.activeArrayName]);
+  }
+}
+
+const project = new ProjectsArray("default");
 
 const primary: MyTask[] = [];
 
@@ -29,7 +56,10 @@ primary.push(task);
 
 function handleProjectEntry(event: SubmitEvent) {
   event.preventDefault();
-  const newPrj = document.getElementById("project-name") as HTMLInputElement;
+  const newPrj = (document.getElementById("project-name") as HTMLInputElement)
+    .value;
+  project.setActiveArray(newPrj);
+  prjctForm.reset();
 }
 
 prjctForm.addEventListener("submit", handleProjectEntry);
@@ -38,25 +68,26 @@ prjctForm.addEventListener("submit", handleProjectEntry);
 
 function handleTaskEntry(event: SubmitEvent) {
   event.preventDefault();
-  const taskTitle = document.getElementById("title") as HTMLInputElement;
-  const taskDescription = document.getElementById(
-    "description"
-  ) as HTMLInputElement;
-  const taskDueDate = document.getElementById("due-date") as HTMLInputElement;
-  const taskPriority = document.getElementById("priority") as HTMLSelectElement;
-  console.log(
-    `${taskTitle.value}  ${taskDescription.value} ${taskDueDate.value}  ${taskPriority.value}`
-  );
+  const taskTitle = (document.getElementById("title") as HTMLInputElement)
+    .value;
+  const taskDescription = (
+    document.getElementById("description") as HTMLInputElement
+  ).value;
+  const taskDueDate = (document.getElementById("due-date") as HTMLInputElement)
+    .value;
+  const taskPriority = (
+    document.getElementById("priority") as HTMLSelectElement
+  ).value;
   const task = new MyTask(
-    taskTitle.value,
-    taskDescription.value,
-    taskDueDate.value,
-    taskPriority.value
+    taskTitle,
+    taskDescription,
+    taskDueDate,
+    taskPriority
   );
-  console.log(task);
-  primary.push(task);
+  // console.log(task);
+  // primary.push(task);
+  project.addValueToActiveArray(task);
   taskForm.reset();
-  // console.log(primary);
 }
 
 taskForm.addEventListener("submit", handleTaskEntry);
